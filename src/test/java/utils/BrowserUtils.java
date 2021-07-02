f3c679e6e762b00c46d45e7b15daf22f293dfbdd
DmitryGorbunov19 <joker7arz@gmail.com>
2021-07-01 20:26:43 -0400

    V1.0 Final set

--- src/test/java/utils/BrowserUtils.java
+++ src/test/java/utils/BrowserUtils.java
@@ -9,6 +9,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 
 public class BrowserUtils {
 
